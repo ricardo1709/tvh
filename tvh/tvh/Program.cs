4d6ebfba02a68c785a745efd6f4a8634b6997fda
lukezedutchman <specularsteam28@outlook.com>
2017-11-14 16:13:09 +0100

    doppediedee

--- tvh/tvh/Program.cs
+++ tvh/tvh/Program.cs
@@ -10,13 +10,31 @@ namespace tvh
     {
         static void Main(string[] args)
         {
+            int schijfs;
+            string input;
+            int steps = 1;
+            int amount = 1;
+            do {
+                Console.WriteLine("Voer het nummer van het aantal schijven in, waarvan je het minimale aantal zetten wilt weten");
+                input = Console.ReadLine();
+            } while (!int.TryParse(input, out schijfs));
+
+            for( amount = schijfs; amount > 0; amount -=1)
+            {
+                steps *= 2;
+            }
+
+
+
+            Console.WriteLine("Het minimale aantal stappen voor is: {0}", steps-1);
             Console.WriteLine("Press enter to exit");
             Console.ReadLine();
         }
 
-        static string Table(string schijfs, string steps)
+        /*static string Table(string schijfs, string steps)
         {
             return string.Format("{0, 20}{1}", schijfs, steps);
-        }
+        }*/
+
     }
 }
