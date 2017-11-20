dc37b7fb4cc65eb7cff0e368c4a224cb98e4ed03
lukezedutchman <specularsteam28@outlook.com>
2017-11-20 15:09:16 +0100

    updated,for-loop,tvh

--- tvh/tvh/Program.cs
+++ tvh/tvh/Program.cs
@@ -13,17 +13,16 @@ namespace tvh
             int schijfs;
             string input;
             ulong steps = 1;
-            int amount = 1;
+            int amount = 0;
 
             do
             {
                 Console.WriteLine("Voer het nummer van het aantal schijven in, waarvan je het minimale aantal zetten wilt weten");
                 input = Console.ReadLine();
 
-
                 if (int.TryParse(input, out schijfs))
                 {
-                    for (amount = schijfs; amount > 0; amount -= 1)
+                    for (; amount < schijfs; amount += 1)
                     {
                         steps *= 2;
                     }
