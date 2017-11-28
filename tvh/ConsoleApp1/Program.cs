4ce17393ecd35921ad78f7b0f423300debf6c4be
lukezedutchman <specularsteam28@outlook.com>
2017-11-28 15:33:55 +0100

    time added to for and to the power of.

--- tvh/ConsoleApp1/Program.cs
+++ tvh/ConsoleApp1/Program.cs
@@ -14,16 +14,33 @@ namespace ConsoleApp1
             string input;
             ulong steps;
             int amount = 2;
+            
 
+            
             do
             {
                 Console.WriteLine("Voer het nummer van het aantal schijven in, waarvan je het minimale aantal zetten wilt weten.");
-                Console.WriteLine("Het getal dat je invoerd mag niet groter zijn dan 63.");
+                Console.WriteLine("Het getal dat je invoerd mag niet groter zijn dan 64.");
                 input = Console.ReadLine();
 
                 if (int.TryParse(input, out schijfs))
                 {
-                    Console.WriteLine("Het minimale aantal zetten voor {0} schijven is: {2}.", input, (steps = (ulong)Math.Pow(amount, schijfs)), steps - 1);
+                    Console.WriteLine("Het minimale aantal zetten voor {0} schijven is: {2}.", input, (steps = (ulong)Math.Pow(amount, schijfs )), steps -1);
+
+                    ulong sec = steps;
+                    ulong min = sec / 60;
+                    ulong hours = min / 60;
+                    ulong days = hours / 24;
+                    ulong years = days / 365;
+                    ulong mill = years / 1000000;
+
+                    sec = steps % 60;
+                    min %= 60;
+                    hours %= 24;
+                    days %= 365;
+                    years %= 1000000;
+                                       
+                    Console.WriteLine("Het duurt je {0}milleniums, {1}jaren, {2}dagen, {3}uren, {4}minuten, {5}seconden, om dit op te lossen", mill, years, days, hours, min, sec);
                     Console.WriteLine("Druk op ENTER om nog een cijfer in te voeren of,");
                     Console.WriteLine("Druk op SPATIE om aftesluiten.");
                 }
@@ -33,8 +50,17 @@ namespace ConsoleApp1
                     continue;
                 }
             } while (Console.ReadKey(true).Key != ConsoleKey.Spacebar); ;
+        }
+
+        static public void Time()
+        {
 
         }
+
+
+
+
+
     }
 }
 
--- tvh/tvh/Program.cs
+++ tvh/tvh/Program.cs
@@ -14,6 +14,7 @@ namespace tvh
             string input;
             ulong steps = 1;
             int amount = 1;
+
             do {
                 Console.WriteLine("Voer het nummer van het aantal schijven in, waarvan je het minimale aantal zetten wilt weten");
                 input = Console.ReadLine();
@@ -22,7 +23,7 @@ namespace tvh
                     for (; amount <= schijfs; amount += 1)
                     {
                         steps *= 2;
-                        Write(amount, steps -1);
+                        Write(schijfs, steps -1);
                     }
                 }
                 else
@@ -33,14 +34,30 @@ namespace tvh
 
             } while (!int.TryParse(input, out schijfs));
 
+
             Console.WriteLine("Het minimale aantal stappen voor {0} schijven is: {1}", input, steps - 1);
+
+            ulong sec = steps;
+            ulong min = sec / 60;
+            ulong hours = min / 60;
+            ulong days = hours / 24;
+            ulong years = days / 365;
+            ulong mill = years / 1000000;
+
+            sec = steps % 60;
+            min %= 60;
+            hours %= 24;
+            days %= 365;
+            years %= 1000000;
+
+            Console.WriteLine("Het duurt je {0}milleniums, {1}jaren, {2}dagen, {3}uren, {4}minuten, {5}seconden, om dit op te lossen", mill, years, days, hours, min, sec);
             Console.WriteLine("Press enter to exit");
             Console.ReadLine();
         }
 
         static public void Write(int schijf, ulong steps)
         {
-            Console.WriteLine("{0, 2}{1, 20}",schijf, steps);
-        } 
+            Console.WriteLine("{0, 3}{1, 26}", schijf, steps);
+        }
     }
 }
